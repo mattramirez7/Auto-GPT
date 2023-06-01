c2a62f6cd8bba6e101c999f1ca7312f41a0d6a12
mattramirez7 <matthewram38@gmail.com>
2023-05-31 17:32:42 -0700

    Added agent_manager edge case tests

--- tests/test_agent_manager.py
+++ tests/test_agent_manager.py
@@ -62,3 +62,32 @@ def test_delete_agent(agent_manager: AgentManager, task, prompt, model):
     success = agent_manager.delete_agent(key)
     assert success
     assert key not in agent_manager.agents
+
+def test_invalid_key_message_agent(agent_manager):
+    # Attempt to send a message to an invalid key
+    invalid_key = 999
+    user_message = "Test message"
+    with pytest.raises(KeyError):
+        agent_reply = agent_manager.message_agent(invalid_key, user_message)
+
+def test_multiple_agents(agent_manager, task, prompt, model):
+    # Test create multiple agents and verify they are in the list_agents() output
+    key1, _ = agent_manager.create_agent(task, prompt, model)
+    key2, _ = agent_manager.create_agent(task, prompt, model)
+    agents_list = agent_manager.list_agents()
+    assert (key1, task) in agents_list
+    assert (key2, task) in agents_list
+    assert len(agents_list) == 2
+
+    # Test delete one agent and assert that it is removed from the list_agents() output
+    agent_manager.delete_agent(key1)
+    agents_list = agent_manager.list_agents()
+    assert (key1, task) not in agents_list
+    assert (key2, task) in agents_list
+    assert len(agents_list) == 1
+
+def test_delete_nonexistent_agent(agent_manager):
+    # Test attempting to delete a nonexistent agent returns False
+    nonexistent_key = 999
+    success = agent_manager.delete_agent(nonexistent_key)
+    assert not success   
